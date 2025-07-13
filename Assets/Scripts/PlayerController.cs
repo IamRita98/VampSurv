ed7cac80b1968f0e29a4a8dfafdd8a9a543e434c
DiazAle5 <youarereta@gmail.com>
2025-07-13 18:49:55 -0400

    Implemented a few things
    But w/ sprite flipper script. It's flipping at world pos 0 not when your mouse is past Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,30 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(Stats))]
+[RequireComponent(typeof(SpriteFlipper))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    private Rigidbody2D rb;
     private Vector2 input;
+    private Rigidbody2D rb;
+
+    private Stats stats;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        stats = GetComponent<Stats>();
     }
 
     private void Update()
     {
         input.x = Input.GetAxisRaw("Horizontal");
-        input.y = Input.GetAxisRaw("Verticl");
+        input.y = Input.GetAxisRaw("Vertical");
         input.Normalize();
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = input * speed;
+        rb.linearVelocity = input * stats.speed;
     }
 }
---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public int hp;
+    public float speed;
+}
