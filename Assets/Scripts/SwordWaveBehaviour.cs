d6f0c786f28a2cf78661c17ea49de724aff8aca1
DiazAle5 <youarereta@gmail.com>
2025-07-15 14:54:16 -0400

    Set up sword wave attack & dictionaries for stats

---
+++ Assets/Scripts/SwordWaveBehaviour.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class SwordWaveBehaviour : MonoBehaviour
+{
+    Timer timer;
+    GetMousePos mousePos;
+
+    Attacks atks;
+
+    public Vector3 targetSize;
+
+    private void Start()
+    {
+        atks = GameObject.FindGameObjectWithTag("Weapon").GetComponent<Attacks>();
+
+        timer = GetComponent<Timer>();
+        mousePos = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GetMousePos>();
+
+        Vector2 dir = mousePos.mousePos - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        targetSize = new Vector3(transform.localScale.x * .75f, transform.localScale.y * 2, 1);
+        timer.SetTimer(atks.projectileDuration);
+    }
+
+    private void Update()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, targetSize, atks.projectileDuration * Time.deltaTime);
+
+        if (!timer.timerComplete) return;
+        Destroy(this.gameObject);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.right * atks.projectileSpeed * Time.deltaTime);
+    }
+}
