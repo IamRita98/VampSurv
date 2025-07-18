9d1d4e2d1ade55b7be25915bef74329c30a8d7ee
DiazAle5 <youarereta@gmail.com>
2025-07-18 15:40:45 -0400

    Moved Health Regen over to cStats

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -15,8 +15,8 @@ public class CharacterStats : MonoBehaviour
     public float projectileDuration;
     public float projectileArea;
 
-    public enum CharacterName 
-    { 
+    public enum CharacterName
+    {
         TempChar,
         FillerChar,
 
@@ -32,6 +32,16 @@ public class CharacterStats : MonoBehaviour
     {
         InstantiateDict();
         currentHP = maxHP;
+    }
+
+    bool isHurt;
+    private void Update()
+    {
+        if (currentHP < maxHP) isHurt = true;
+        else isHurt = false;
+
+        if (!isHurt) return;
+        HPRegen();
     }
 
     private void SetStats(CharacterName charName)
@@ -54,30 +64,57 @@ public class CharacterStats : MonoBehaviour
         //Then when they get a buff thats +20% attack speed we can apply it as * 1.2 to this stat
         //PC's
         charStats.Add(CharacterName.TempChar, new StatConstructor
-            (health: 100, healthRegen: .2f, spd: 3, 
+            (health: 100, healthRegen: .2f, spd: 3,
             abilityCD: 1, atkSpeed: 1, dmg: 1, projSpeed: 1, projDur: 1, projArea: 1));
 
 
 
         //Enemies
         charStats.Add(CharacterName.Zombie, new StatConstructor
-            (health: 25, healthRegen: 0, spd: 2f, 
+            (health: 25, healthRegen: 0, spd: 1.75f,
             abilityCD: 1, atkSpeed: 1, dmg: 5, projSpeed: 1, projDur: 1, projArea: 1));
 
         charStats.Add(CharacterName.TankyZombie, new StatConstructor
-            (health: 55, healthRegen: 0, spd: 1.5f, 
+            (health: 55, healthRegen: 0, spd: 1.4f,
             abilityCD: 1, atkSpeed: 1, dmg: 10, projSpeed: 1, projDur: 1, projArea: 1));
 
 
         SetStats(charName);
     }
 
+    void HPRegen()
+    {
+        ChangeHP(-hpRegen * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Change current hp, pass a negative number for healing
+    /// </summary>
+    /// <param name="dmgTaken"></param>
     public void ChangeHP(float dmgTaken)
     {
         if (dmgTaken < 0 && currentHP >= maxHP) return; //If dmg taken is healing and our current HP is capped, return
-        
+
         currentHP -= dmgTaken;
         if (currentHP > maxHP) currentHP = maxHP;
-        print(currentHP + "/" + maxHP);
+        if(dmgTaken >= 1)
+        {
+            print(currentHP + "/" + maxHP);
+        }
+
+    }
+
+    public void ChangeHPRengen(float amountToChange)
+    {
+        hpRegen += amountToChange;
+    }
+
+    /// <summary>
+    /// Change base dmg of Char. This is percentage base so number passed should be 1.2 for 20% more or .8 for 20% less
+    /// </summary>
+    /// <param name="amountToChange"></param>
+    public void ChangeDamage(float amountToChange)
+    {
+        damage *= amountToChange;
     }
 }
